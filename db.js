9e33c321f24601ff8fbd5ea59404d199fe99b3fe
Chahat2005 <chahatv210@gmail.com>
2025-09-19 11:26:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@
 // const connection =mysql.createConnection({
 //     host: 'localhost',
 //     user: 'root',
-//     password: 'Chahat@123',
+//     password: 'password',
 //     database: 'smart_home'
 // });
 
@@ -14,3 +14,4 @@
 // });
 
 // module.exports = connection;
+
